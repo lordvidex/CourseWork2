0673e7898fcb6813d578933317b9d7364fb64cce
lordvidex <lordvidex.prime@gmail.com>
2021-09-08 09:45:54 +0300

    Created new repo, added first lab work and second lab work

---
+++ Lab1/src/DataGetter.java
@@ -0,0 +1,15 @@
+import Models.DataTicket;
+
+/**
+ * Created by IntelliJ IDEA
+ * Date: 01.09.2021
+ * Time: 3:32 PM
+ *
+ * @author lordvidex
+ * Name: Овамойо Олувадамилола Эванс
+ * <p>
+ * Desc:
+ */
+public interface DataGetter {
+    Double getData(DataTicket ticket);
+}
---
+++ Lab2/src/server/Server.java
@@ -0,0 +1,66 @@
+package server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+
+public class Server {
+
+    private static final ArrayList<MicroServer> microServers = new ArrayList<>();
+    private static ServerSocket server; // серверсокет
+
+    private static void close() throws IOException {
+        for(MicroServer m: microServers) {
+            if(!m.isClosed()) m.close();
+        }
+        server.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            try {
+                server = new ServerSocket(10000); // серверсокет прослушивает порт 4004
+                System.out.println("Сервер запущен в порт 10000"); // хорошо бы серверу
+
+                // an open thread that listens for server requests
+                Thread quitThread = new Thread(() -> {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                    try {
+                        while(true) {
+                            String line = br.readLine();
+                            if(line.equals("/quit")) {
+                                close();
+                                break;
+                            } else {
+                                System.out.println("Enter /quit to stop server");
+                            }
+                        }
+                    }catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                quitThread.start();
+                while(!server.isClosed()) {
+                    //   объявить о своем запуске
+                    try {
+                        Socket clientSocket = server.accept();
+                        MicroServer newClient = new MicroServer(clientSocket);
+                        new Thread(newClient).start(); // начинает новый сервер
+
+                        microServers.add(newClient); // сохранить в массив
+                        System.out.println(newClient + " connected");
+                    } catch (SocketException err){
+                        break;
+                    }
+                }
+
+            } finally {
+                System.out.println("Сервер закрыт!");
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
