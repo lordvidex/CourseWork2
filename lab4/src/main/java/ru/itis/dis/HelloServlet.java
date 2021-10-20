eb59a1d0ba1d850895bec69e39d598ade9bf30a9
lordvidex <lordvidex.prime@gmail.com>
2021-10-20 15:39:49 +0300

    Working css uploads

---
+++ lab4/src/main/java/ru/itis/dis/HelloServlet.java
@@ -0,0 +1,19 @@
+package ru.itis.dis;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html");
+        request.getRequestDispatcher("html/index.html").forward(request,response);
+    }
+}
