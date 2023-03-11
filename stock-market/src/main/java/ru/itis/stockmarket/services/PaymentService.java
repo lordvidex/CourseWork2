399a8f9409174133a08726471f2142cb1bc66339
Evans Owamoyo <lordvidex.prime@gmail.com>
2023-03-11 04:51:19 +0300

    added stock-market as a submodule instead of copy hack

--- stock-market/src/main/java/ru/itis/stockmarket/services/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.stockmarket.services;
-
-import ru.itis.stockmarket.dtos.InnerIdResponseDto;
-
-import java.util.UUID;
-
-public interface PaymentService {
-    InnerIdResponseDto makePaymentForContract(UUID contractId);
-}
--- stock-market/src/main/resources/db/migration/V2__remove_tables.sql
+++
@@ -1,3 +0,0 @@
--- remove stale tables
-DROP TABLE IF EXISTS product_tag CASCADE ;
-DROP TABLE IF EXISTS product_tag_products CASCADE;
